f2997d89502287430245ef27998726a4907c4572
S Tanveer Hussain <nabeel12147277@gmail.com>
2026-01-30 15:52:20 +0500

    Initial Commit

---
+++ MovieExplorer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MovieExplorer
+//
+//  Created by syed tanveer hussain on 30/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MovieExplorer/MovieExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieExplorerApp.swift
+//  MovieExplorer
+//
+//  Created by syed tanveer hussain on 30/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct MovieExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
